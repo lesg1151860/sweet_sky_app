8feccd74f12e7cafa4bbeedccd206ff1a8513497
lesg1151860 <luiseduardosg@ufps.edu.co>
2025-05-12 16:51:06 -0500

    Creación del modelo de salsa

---
+++ app/Backend/src/models/Sauce.ts
@@ -0,0 +1,37 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/database';
+
+class Sauce extends Model {
+  public id!: number;
+  public name!: string;
+  public isActive!: boolean;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+Sauce.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Sauce',
+    tableName: 'sauces',
+  }
+);
+
+export default Sauce; 
